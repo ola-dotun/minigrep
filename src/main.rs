726f1cf22cf4eb1b23f1e2f163361d9e9cf4cd56
Dotun Kola-Olaleye <dotunkolaolaleye@gmail.com>
2025-05-04 22:58:25 -0600

    Better error handling and process exit

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::{env, fs, process};
+use std::error::Error;
+
 struct Config {
     query: String,
     file_path: String,
@@ -9,7 +11,7 @@ impl Config {
         if args.len() < 3 {
             return Err("Not enough arguments");
         }
-        
+
         let query = args[1].clone();
         let file_path = args[2].clone();
         Ok(Config { query, file_path })
@@ -26,6 +28,15 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(config.file_path).expect("Something went wrong reading the file");
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
     println!("With text:\n{contents}");
+    
+    Ok(())
 }
