bcb4e17b56892805b5056ea62449e824ed72aaf8
Dotun Kola-Olaleye <dotunkolaolaleye@gmail.com>
2025-05-04 21:05:06 -0600

    Checking args length

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
 use std::{env, fs};
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("Not enough arguments");
+        }
+        
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Config { query, file_path }
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::new(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path).expect("Something went wrong reading the file");
+    let contents = fs::read_to_string(config.file_path).expect("Something went wrong reading the file");
     println!("With text:\n{contents}");
 }
