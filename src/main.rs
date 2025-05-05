eb8268e45f173e0031490799257140004ed0227e
Dotun Kola-Olaleye <dotunkolaolaleye@gmail.com>
2025-05-05 02:54:25 -0600

    With unit test

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,13 @@
-use std::{env, process};
 use minigrep::Config;
+use std::{env, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing args: {err}");
+    let config = Config::build(&args).unwrap_or_else(|_err| {
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
-
-    if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+    if let Err(_e) = minigrep::run(config) {
         process::exit(1);
     }
 }
