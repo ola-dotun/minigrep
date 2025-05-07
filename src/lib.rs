acc9b3258e8c61ddd9736d72fbd0e2dc10bbb2a5
Dotun Kola-Olaleye <dotunkolaolaleye@gmail.com>
2025-05-07 01:10:51 -0600

    IGNORE_CASE from either command line or env variable

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,14 @@
+use std::env;
 use std::error::Error;
 use std::fs;
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
+
+const IGNORE_CASE: &str = "IGNORE_CASE";
 
 impl Config {
     pub fn build(args: &[String]) -> Result<Config, &'static str> {
@@ -14,14 +18,27 @@ impl Config {
 
         let query = args[1].clone();
         let file_path = args[2].clone();
-        Ok(Config { query, file_path })
+        let ignore_case =
+            env::var(IGNORE_CASE).is_ok() || (args.len() == 4 && args[3] == IGNORE_CASE);
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{line}");
     }
 
